18cd8e4032a0dab99f08ab944d4b34779b558981
KhadeejaSshah <khadeejaishaque03@gmail.com>
2026-03-10 21:09:54 +0500

    again for image.

--- src/App.tsx
+++ src/App.tsx
@@ -541,7 +541,7 @@ export default function App() {
                 desc: "Bachelor of Data Science", 
                 icon: GraduationCap, 
                 color: "text-cyber-blue", 
-                img: "src/public/assets/graduation.jpeg",
+                img: "public/assets/graduation.jpeg",
                 onClick: () => setShowGradStory(true)
               },
               { 
